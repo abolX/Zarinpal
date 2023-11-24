4d10fe3a3430a8ea8bc636cc94dde6e1733196a6
Abolfazl Amiri <107195131+abolX@users.noreply.github.com>
2023-11-24 14:33:44 +0330

    Add files via upload

---
+++ zarinpal/methods/request.py
@@ -0,0 +1,42 @@
+from typing import Union, Optional
+
+import _zarinpal
+from ..objects import Data
+
+
+class Request:
+    
+    async def request(
+            self: "zarinpal.Zarinpal",
+            amount: Union[int, str],
+            description: str,
+            callback_url: str,
+            currency: Union["IRR", "IRT"] = None,
+            metadata: Optional["object.MetaData"] = None
+    ) -> "object.Data":
+        
+        if int(amount) < 1_000 or int(amount) > 1_000_000_000:
+            raise ValueError(
+                "the amount may not be greater than 1,000,000,000 "
+                "and be at least 1,000"
+            )
+        json = locals()
+        del json["self"]
+        
+        if isinstance(currency, str):
+            currency = currency.upper()
+            if currency not in ["IRR", "IRT"]:
+                raise KeyError("currency must be IRR or IRT")
+            json["currency"] = currency
+            
+        if len(description) >= 500:
+            raise ValueError(
+                "the description must be less than 500 characters"
+            )
+        if metadata:
+            json["metadata"] = metadata.to_dict()
+        
+        json["merchant_id"] = self.merchant_id
+        result = await self.connection.execute("request", json)
+        return Data(**result)
+ 
---
+++ zarinpal/methods/verify.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from ..objects import Data
+import _zarinpal
+
+
+class Verify:
+    
+    async def verify(
+            self: "zarinpal.Zarinpal",
+            amount: Union[int, str],
+            authority: str
+    ) -> "object.Data":
+        
+        json = locals() 
+        del json["self"]
+        json["merchant_id"] = self.merchant_id
+        result = await self.connection.execute("verify", json)
+        return Data(**json, **result)
